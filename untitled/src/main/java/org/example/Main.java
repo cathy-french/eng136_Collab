c632e52c0c4aebfada9385135317e66143d83eda
Rumana <rumana.miah@hotmail.com>
2022-11-02 11:57:56 +0000

    New changes to main

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -5,5 +5,7 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println("Hello from Cathy!");
+        System.out.println("Hey guysss its meeee");
+        System.out.println("sooouuuup");
     }
 }
